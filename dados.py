643f82ff940e6ba799be358ccbb0c1333c5ce34b
Helder Martins de Sousa <heldermartinsdesousa@gmail.com>
2023-10-11 21:24:17 -0300

    first commit

---
+++ dados.py
@@ -0,0 +1,11 @@
+# Crie um arquivo chamado dados.py que insira ao menos 5 linhas em cada tabela, e 10 linhas na tabela de vendas. 
+# Na tabela de histórico de preços, coloque o preço atual e a data atual de cada produto criado. 
+
+# Procure inserir informações de forma que as consultas a seguir retornem algum resultado.
+
+# Crie um arquivo chamado consultas.py que responda às consultas a seguir:
+# 1 - Liste todo o histórico de preços (valor, data) do produto "Caneta Bic esferográfica azul".
+
+# 2 - Liste descrição e preço de todos os produtos da categoria "Papelaria", ordenados no menor para o maior preço.
+# 3 - Recupere os nomes dos clientes que fizeram ao menos uma compra com valor superior a R$ 5.000,00 no mês de setembro/2022. 
+#Exiba-os em ordem alfabética.
---
+++ esquema.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from peewee import PostgresqlDatabase, Model, TextField, FloatField, IntegerField, ForeignKeyField, DateTimeField, PrimaryKeyField
+
+db = PostgresqlDatabase('peewee_bd2',port=5432,user='postgres',password='Helder@1992')
+
+class BaseModel(Model):
+    class Meta():
+        database = db
+
+class CATEGORIA(BaseModel):
+    id = PrimaryKeyField
+    descricao = TextField
+    
+class CLIENTE(BaseModel):
+    id = PrimaryKeyField
+    nome = TextField
+    endereco = TextField
+    data_registro = DateTimeField(default = datetime.now)
+
+class PRODUTOS(BaseModel):
+    id = PrimaryKeyField
+    descricao = TextField
+    id_categoria = ForeignKeyField(CATEGORIA, backref='produtos')
+    valor = FloatField
+    
+class HISTORICO_PRECOS(BaseModel):
+    id = PrimaryKeyField
+    id_produto = ForeignKeyField(PRODUTOS, backref='historicoprecoprodutos')
+    valor = FloatField
+    data = DateTimeField(default = datetime.now)
+    
+class VENDAS(BaseModel):
+    id = PrimaryKeyField
+    id_produto = ForeignKeyField(PRODUTOS, backref='vendasprodutos')
+    id_cliente = ForeignKeyField(CLIENTE, backref='vendascliente')
+    data = DateTimeField(default = datetime.now)
+    quantidade = IntegerField
+    valor_unitario = FloatField
+    valor_total = FloatField
+
+db.create_tables[CATEGORIA, CLIENTE, PRODUTOS, HISTORICO_PRECOS,VENDAS]
+
